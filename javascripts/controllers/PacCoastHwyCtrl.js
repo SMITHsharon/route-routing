aa35d7b5ad515ab98c3b4b395f4af7fc3589251a
sharon smith <sharon.e.smith2@gmail.com>
2017-05-23 19:08:34 -0500

    changes made w Taylor

--- javascripts/controllers/PacCoastHwyCtrl.js
+++ javascripts/controllers/PacCoastHwyCtrl.js
@@ -2,6 +2,7 @@ app.controller("PacCoastHwyCtrl", function($location, $scope) {
 
 	$scope.highwayDescription = false;
 	$scope.highwayName = "Pacific Coast Highway";
+	$scope.background = "/images/Pacific%20Coast%20Highway%201.jpg";
 
 	$scope.showDescription = () => {
 		$scope.highwayDescription = true;
--- javascripts/controllers/Route66Ctrl.js
+++ javascripts/controllers/Route66Ctrl.js
@@ -2,6 +2,7 @@ app.controller("Route66Ctrl", function($location, $scope) {
 
 	$scope.highwayDescription = false;
 	$scope.highwayName = "Route 66";
+	$scope.background = "/images/route%2066.jpeg";
 
 	$scope.showDescription = () => {
 		$scope.highwayDescription = true;
